7302118c455e18931d7d51f81bc23d6a51c797ca
vedavyas <vedavyas14042003@gmail.com>
2023-12-28 20:15:27 +0530

    compleated companys part

---
+++ src/components/companies.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <div class="companys">
+    <h2>Trusted by</h2>
+    <div class="companyImages">
+      <img src="@/assets/Infosys_logo 1.svg" alt="company1" />
+      <img src="@/assets/company2.svg" alt="company2" />
+      <img src="@/assets/company3.svg" alt="company3" />
+      <img src="@/assets/company4.svg" alt="company4" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.companys {
+  width: 65%;
+  margin-inline: auto;
+  margin-top: 200px;
+  margin-bottom: 100px;
+  text-align: center;
+}
+.companys > h2 {
+  color: var(--vt-c-action-color);
+  font-size: 38px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.583px;
+  margin-bottom: 21px;
+}
+.companyImages {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import companies from '@/components/companies.vue';
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <companies />
   </main>
 </template>
