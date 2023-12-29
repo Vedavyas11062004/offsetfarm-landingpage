239ccd14c5b511293dd944cd8b1529c771b969b7
vedavyas <vedavyas14042003@gmail.com>
2023-12-29 22:24:06 +0530

    Added scoll button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import companies from '@/components/companies.vue';
-import products from '@/components/products.vue';
-import features from '@/components/features.vue';
-import dynamicAssesment from '@/components/dynamicAssesment.vue';
-import customerMapping from '@/components/customerMapping.vue';
-import intutiveDesign from '@/components/intutiveDesigns.vue';
-import reporting from '@/components/reporting.vue'
-import reviews from '@/components/reviews.vue';
-import industries from '@/components/industries.vue';
-import footerContainer from '@/components/footerContainer.vue';
+import TheWelcome from "../components/TheWelcome.vue";
+import companies from "@/components/companies.vue";
+import products from "@/components/products.vue";
+import features from "@/components/features.vue";
+import dynamicAssesment from "@/components/dynamicAssesment.vue";
+import customerMapping from "@/components/customerMapping.vue";
+import intutiveDesign from "@/components/intutiveDesigns.vue";
+import reporting from "@/components/reporting.vue";
+import reviews from "@/components/reviews.vue";
+import industries from "@/components/industries.vue";
+import footerContainer from "@/components/footerContainer.vue";
+import { ref } from "vue";
+
+const scrollToHero = () => {
+  window.scrollTo(0, 0);
+};
 </script>
 
 <template>
@@ -18,12 +23,26 @@ import footerContainer from '@/components/footerContainer.vue';
     <companies />
     <products />
     <features />
-    <dynamicAssesment /> 
-    <customerMapping /> 
-    <intutiveDesign /> 
-    <reporting /> 
-    <reviews/>
-    <industries/>
-    <footerContainer/>
+    <dynamicAssesment />
+    <customerMapping />
+    <intutiveDesign />
+    <reporting />
+    <reviews />
+    <industries />
+    <footerContainer />
+    <div class="btn" @click="scrollToHero">
+      <img src="@/assets/upArrow.svg" alt="arrow" />
+    </div>
   </main>
 </template>
+
+<style scoped>
+.btn {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+}
+.btn:hover {
+  cursor: pointer;
+}
+</style>
